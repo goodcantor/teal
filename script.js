880701892da974f5d811b6b0b0ebed680aa11bb0
Daniil Apekalovskii <goodcantor@mail.ru>
2023-02-22 18:50:14 +0300

    almost done

--- script.js
+++ script.js
@@ -7,59 +7,115 @@ burger.onclick = function () {
   html.classList.toggle("scroll-hidden");
 };
 
-LottieScrollTrigger({
-  target: "#animationWindow",
-  path: "imgs/lottie.json",
-  // path: "https://lottie.host/4acc2fee-e4c9-4ef1-bb0c-14b3b3e6b51b/7jVqkAD5kn.json",
-  // path: "https://assets.codepen.io/35984/tapered_hello.json",
-  speed: "slow",
-  scrub: true, // seconds it takes for the playhead to "catch up"
-  // you can also add ANY ScrollTrigger values here too, like trigger, start, end, onEnter, onLeave, onUpdate, etc. See https://greensock.com/docs/v3/Plugins/ScrollTrigger
-});
-
-function LottieScrollTrigger(vars) {
-  let playhead = { frame: 0 },
-    target = gsap.utils.toArray(vars.target)[0],
-    speeds = { slow: "+=3000", medium: "+=1000", fast: "+=500" },
-    st = {
-      trigger: target,
-      pin: true,
-      start: "top top",
-      end: speeds[vars.speed] || "+=1000",
-      scrub: 1,
-    },
-    ctx = gsap.context && gsap.context(),
-    animation = lottie.loadAnimation({
-      container: target,
-      renderer: vars.renderer || "svg",
-      loop: false,
-      autoplay: false,
-      path: vars.path,
-      rendererSettings: vars.rendererSettings || {
-        preserveAspectRatio: "xMidYMid slice",
-      },
-    });
-  for (let p in vars) {
-    // let users override the ScrollTrigger defaults
-    st[p] = vars[p];
-  }
-  animation.addEventListener("DOMLoaded", function () {
-    let createTween = function () {
-      animation.frameTween = gsap.to(playhead, {
-        frame: animation.totalFrames - 1,
-        ease: "none",
-        onUpdate: () => animation.goToAndStop(playhead.frame, true),
-        scrollTrigger: st,
-      });
-      return () => animation.destroy && animation.destroy();
-    };
-    ctx && ctx.add ? ctx.add(createTween) : createTween();
-    // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
-    ScrollTrigger.sort();
-    ScrollTrigger.refresh();
+if (document.body.clientWidth > 800) {
+  LottieScrollTrigger({
+    target: "#animationWindow",
+    path: "imgs/lottie.json",
+    // path: "https://lottie.host/4acc2fee-e4c9-4ef1-bb0c-14b3b3e6b51b/7jVqkAD5kn.json",
+    // path: "https://assets.codepen.io/35984/tapered_hello.json",
+    speed: "slow",
+    scrub: true, // seconds it takes for the playhead to "catch up"
+    // you can also add ANY ScrollTrigger values here too, like trigger, start, end, onEnter, onLeave, onUpdate, etc. See https://greensock.com/docs/v3/Plugins/ScrollTrigger
   });
-  return animation;
+
+  function LottieScrollTrigger(vars) {
+    let playhead = { frame: 0 },
+      target = gsap.utils.toArray(vars.target)[0],
+      speeds = { slow: "+=3000", medium: "+=1000", fast: "+=500" },
+      st = {
+        trigger: target,
+        pin: true,
+        start: "top top",
+        end: speeds[vars.speed] || "+=1000",
+        scrub: 1,
+      },
+      ctx = gsap.context && gsap.context(),
+      animation = lottie.loadAnimation({
+        container: target,
+        renderer: vars.renderer || "svg",
+        loop: false,
+        autoplay: false,
+        path: vars.path,
+        rendererSettings: vars.rendererSettings || {
+          preserveAspectRatio: "xMidYMid slice",
+        },
+      });
+    for (let p in vars) {
+      // let users override the ScrollTrigger defaults
+      st[p] = vars[p];
+    }
+    animation.addEventListener("DOMLoaded", function () {
+      let createTween = function () {
+        animation.frameTween = gsap.to(playhead, {
+          frame: animation.totalFrames - 1,
+          ease: "none",
+          onUpdate: () => animation.goToAndStop(playhead.frame, true),
+          scrollTrigger: st,
+        });
+        return () => animation.destroy && animation.destroy();
+      };
+      ctx && ctx.add ? ctx.add(createTween) : createTween();
+      // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
+      ScrollTrigger.sort();
+      ScrollTrigger.refresh();
+    });
+    return animation;
+  }
 }
+
+// LottieScrollTrigger({
+//   target: "#animationWindow",
+//   path: "imgs/lottie.json",
+//   // path: "https://lottie.host/4acc2fee-e4c9-4ef1-bb0c-14b3b3e6b51b/7jVqkAD5kn.json",
+//   // path: "https://assets.codepen.io/35984/tapered_hello.json",
+//   speed: "slow",
+//   scrub: true, // seconds it takes for the playhead to "catch up"
+//   // you can also add ANY ScrollTrigger values here too, like trigger, start, end, onEnter, onLeave, onUpdate, etc. See https://greensock.com/docs/v3/Plugins/ScrollTrigger
+// });
+
+// function LottieScrollTrigger(vars) {
+//   let playhead = { frame: 0 },
+//     target = gsap.utils.toArray(vars.target)[0],
+//     speeds = { slow: "+=3000", medium: "+=1000", fast: "+=500" },
+//     st = {
+//       trigger: target,
+//       pin: true,
+//       start: "top top",
+//       end: speeds[vars.speed] || "+=1000",
+//       scrub: 1,
+//     },
+//     ctx = gsap.context && gsap.context(),
+//     animation = lottie.loadAnimation({
+//       container: target,
+//       renderer: vars.renderer || "svg",
+//       loop: false,
+//       autoplay: false,
+//       path: vars.path,
+//       rendererSettings: vars.rendererSettings || {
+//         preserveAspectRatio: "xMidYMid slice",
+//       },
+//     });
+//   for (let p in vars) {
+//     // let users override the ScrollTrigger defaults
+//     st[p] = vars[p];
+//   }
+//   animation.addEventListener("DOMLoaded", function () {
+//     let createTween = function () {
+//       animation.frameTween = gsap.to(playhead, {
+//         frame: animation.totalFrames - 1,
+//         ease: "none",
+//         onUpdate: () => animation.goToAndStop(playhead.frame, true),
+//         scrollTrigger: st,
+//       });
+//       return () => animation.destroy && animation.destroy();
+//     };
+//     ctx && ctx.add ? ctx.add(createTween) : createTween();
+//     // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
+//     ScrollTrigger.sort();
+//     ScrollTrigger.refresh();
+//   });
+//   return animation;
+// }
 
 // let mm = gsap.matchMedia();
 
